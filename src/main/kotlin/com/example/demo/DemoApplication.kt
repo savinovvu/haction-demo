ac2d3653a1f3c5d0ae45a6d1f9d1d5546858e2d7
Vasiliy Savinov <savinov_vu@inbox.ru>
2025-12-24 17:30:38 +0800

    init

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,8 +1,12 @@
 package com.example.demo
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,4 +14,11 @@ class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+}
+
+@Bean
+fun jsonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
+    return Jackson2ObjectMapperBuilderCustomizer { builder: Jackson2ObjectMapperBuilder ->
+        builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+    }
 }
